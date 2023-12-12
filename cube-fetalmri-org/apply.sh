c0ad612ae398e5a5cdd22bbac7a1ddd8327b9c9c
Jennings Zhang <jenni_zh@protonmail.com>
2023-12-12 15:44:23 -0500

    Add fetalmri.org

---
+++ cube-fetalmri-org/apply.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+HERE="$(dirname "$(readlink -f "$0")")"
+set -ex
+cd "$HERE"
+exec helm upgrade -n hosting-of-medical-image-analysis-platform-dcb83b -f values.yaml cube-fetalmri-org fnndsc/chris --version 0.3.0
+
