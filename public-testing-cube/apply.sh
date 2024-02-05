0712dbde0334b9f3df0e20b79f39587139c9f5d1
Jennings Zhang <jenni_zh@protonmail.com>
2024-02-05 16:46:18 -0500

    Add new CUBE for public testing

---
+++ public-testing-cube/apply.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+HERE="$(dirname "$(readlink -f "$0")")"
+set -ex
+cd "$HERE"
+exec helm upgrade -n hosting-of-medical-image-analysis-platform-dcb83b -f values.yaml public-testing fnndsc/chris --version 0.3.1
